42a2416fa9cf16e35aa178888adc63c5150ca554
FernandoGon19 <129014797+FernandoGon19@users.noreply.github.com>
2023-10-23 21:37:36 -0300

    add arquivos

---
+++ dist/index.f75de5e1.js
@@ -0,0 +1,21 @@
+const dataDoEvento = new Date("Feb 04, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timestampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timestampAtual;
+    const diasEmMs = 86400000;
+    const horaEmMs = 3600000;
+    const minutoEmMs = 60000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diasEmMs / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutoEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutoEmMs / 1000);
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "Evento expirado";
+    }
+}, 1000);
+
+//# sourceMappingURL=index.f75de5e1.js.map
